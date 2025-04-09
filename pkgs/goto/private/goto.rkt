1fd3aec536a23d245aa06b6b5891d93cdaf3daa0
NoahStoryM <noahstorym@gmail.com>
2025-04-09 13:54:55 +0800

    Add `prop:procedure` to `label`

--- pkgs/goto/private/goto.rkt
+++ pkgs/goto/private/goto.rkt
@@ -2,13 +2,13 @@
 
 (provide label? (rename-out [make-label label]) goto current-continuation)
 
-(struct label (k))
+(struct label (k) #:property prop:procedure (struct-field-index k))
 (define (make-label [prompt-tag (default-continuation-prompt-tag)])
   (call/cc goto* prompt-tag))
 (define current-continuation
   (case-λ
     [() (make-label)]
     [(l) (current-continuation l l)]
-    [(l0 l1) ((label-k l0) l1)]))
+    [(l0 l1) (l0 l1)]))
 (define (goto l0 [l1 l0]) (current-continuation l0 l1))
 (define goto* (compose1 current-continuation label))
