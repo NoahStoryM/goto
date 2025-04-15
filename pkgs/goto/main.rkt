214fba4f31f6db6366927452c52bfbe07e6001aa
NoahStoryM <noahstorym@gmail.com>
2025-04-15 15:12:49 +0800

    Avoid unnecessary checks

--- pkgs/goto/main.rkt
+++ pkgs/goto/main.rkt
@@ -2,9 +2,9 @@
 
 (require racket/contract/base)
 (require (contract-in "no-check.rkt"
-                      [label (->* () (continuation-prompt-tag?) continuation?)]
+                      [label (->* () (continuation-prompt-tag?) any)]
                       [goto (->* (continuation?) (continuation?) none/c)]
-                      [current-continuation (case-> (-> continuation?)
+                      [current-continuation (case-> (-> any)
                                                     (-> continuation? none/c)
                                                     (-> continuation? continuation? none/c))]))
 (provide label goto current-continuation (rename-out [current-continuation cc]))
