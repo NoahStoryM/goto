ea92e918b315229f043e4fec4397995d12f9723c
NoahStoryM <noahstorym@gmail.com>
2025-04-17 14:23:44 +0800

    Provide `Goto`

--- pkgs/typed-goto/goto/main.rkt
+++ pkgs/typed-goto/goto/main.rkt
@@ -1,8 +1,11 @@
 #lang typed/racket/base
 
-(require typed/racket/unsafe "private/types.rkt")
+(require "private/types.rkt")
+(provide Goto Label)
+
+(require typed/racket/unsafe)
 (unsafe-require/typed/provide goto/no-check
   [label (∀ (a ...) (→* () (Prompt-TagTop) (→ a ... Nothing)))]
   [goto (→ Label Nothing)]
   [current-continuation (∀ (a ...) (case→ (→ (→ a ... Nothing)) (→ Label Nothing)))])
-(provide Label (rename-out [current-continuation cc]))
+(provide (rename-out [current-continuation cc]))
