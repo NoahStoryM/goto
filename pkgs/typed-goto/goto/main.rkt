f783e2eb06322c27545b1561e08d989a805f2bf9
NoahStoryM <NoahStoryM@gmail.com>
2025-05-25 15:26:34 +0800

    Using `typed/racket/base/optional` instead of `typed/racket/unsafe`

--- pkgs/typed-goto/goto/main.rkt
+++ pkgs/typed-goto/goto/main.rkt
@@ -1,10 +1,9 @@
-#lang typed/racket/base
+#lang typed/racket/base/optional
 
 (require "private/types.rkt")
 (provide Goto Label)
 
-(require typed/racket/unsafe)
-(unsafe-require/typed/provide goto/no-check
+(require/typed/provide goto/no-check
   [label (∀ (a ...) (→* () (Prompt-TagTop) (→ a ... Nothing)))]
   [goto Label]
   [current-continuation (∀ (a ...) (case→ (→ (→ a ... Nothing)) (→ Label Nothing)))])
