8111ff267acbc163db4040c0e9c07a6610e86134
NoahStoryM <NoahStoryM@gmail.com>
2025-06-16 09:18:12 +0800

    Add contract to `goto`

--- pkgs/goto/main.rkt
+++ pkgs/goto/main.rkt
@@ -1,10 +1,14 @@
 #lang racket/base
 
-(require racket/contract/base)
-(require (contract-in "no-check.rkt"
+(require racket/contract/base
+         (contract-in "no-check.rkt"
                       [label (->* () (continuation-prompt-tag?) any)]
                       (rename cc goto (-> continuation? none/c))
-                      [current-continuation (case-> (-> any)
-                                                    (-> continuation? none/c))]))
+                      [current-continuation (case-> (-> any) (-> continuation? none/c))]))
+
 (define goto* (let/cc return (goto (call/cc return))))
-(provide label current-continuation (rename-out [current-continuation cc] [goto* goto]))
+
+(provide label
+         current-continuation
+         (rename-out [current-continuation cc])
+         (contract-out (rename goto* goto continuation?)))
